24e737244b153148702fa901fef4f1ac5ff41e9d
Vinicius Boscoa <virb30@gmail.com>
2021-05-30 15:17:56 -0300

    Initial Commit

---
+++ api/posts.ts
@@ -0,0 +1,17 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+const api = new GhostContentAPI({
+	url: 'https://blog.viniboscoa.dev',
+	key: 'a2d5da77d48c913e371f1f1805',
+	version: 'v3',
+});
+
+export async function getPosts () {
+	return await api.posts
+		.browse({
+			limit: 'all',
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+}
---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+export default {
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'blog',
+		htmlAttrs: {
+			lang: 'en',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [
+			{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+			{ href: 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;700&display=swap', rel: 'stylesheet' },
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+		],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+		'~/assets/css/global.css',
+	],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+	],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		'@nuxt/typescript-build',
+		// https://go.nuxtjs.dev/tailwindcss
+		'@nuxtjs/tailwindcss',
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+	],
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+	},
+
+	tailwindcss: {
+		viewer: false,
+	},
+};
