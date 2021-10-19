b28bb120019ed94d3b4eebb3fada08ee5f5b516b
Vinicius Boscoa <virb30@gmail.com>
2021-10-18 23:49:59 -0300

    chore:remove console.log

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -91,10 +91,6 @@ export default Vue.extend<Data, unknown, Computed, []>({
 		const post = await getPostBySlug(params.slug);
 		return { post };
 	},
-	mounted() {
-		console.log('mounted');
-		console.log(this.post);
-	}
 });
 </script>
 
