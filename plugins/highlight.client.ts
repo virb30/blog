a2df1f6a06c5eb4941c8515ef76056349c9292a4
Vinicius Boscoa <virb30@gmail.com>
2023-12-29 01:38:39 -0300

    chore(highlighter): register typescript language to hightlighter

--- plugins/highlight.client.ts
+++ plugins/highlight.client.ts
@@ -1,19 +1,21 @@
 import { defineNuxtPlugin } from "nuxt/app";
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
 import php from 'highlight.js/lib/languages/php';
 import python from 'highlight.js/lib/languages/python';
 
 
 export default defineNuxtPlugin((nuxtApp) => {
-    hljs.registerLanguage('javascript', javascript);
-    hljs.registerLanguage('python', python);
-    hljs.registerLanguage('php', php);
-    return {
-        provide: {
-            highlighter: hljs
-        }
-    }
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	hljs.registerLanguage('python', python);
+	hljs.registerLanguage('php', php);
+	return {
+		provide: {
+			highlighter: hljs
+		}
+	}
 })
 
 
